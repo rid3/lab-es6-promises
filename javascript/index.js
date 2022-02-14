6f1ad7a5299329d86346192478b359a15c6768b5
artware-solutions <info@artware.solutions>
2022-02-14 18:50:38 +0100

    Solved Iteration 3

--- javascript/index.js
+++ javascript/index.js
@@ -85,7 +85,31 @@ obtainInstruction('steak', 0)
 
 
 // Iteration 3 using async/await
+async function makeBroccoli() {
+  // ... Your code here
+  try {
+    const response1 = await obtainInstruction("broccoli", 0)
+    document.querySelector("#broccoli").innerHTML += `<li>${response1}</li>`
+    const response2 = await obtainInstruction("broccoli", 1)
+    document.querySelector("#broccoli").innerHTML += `<li>${response2}</li>`
+    const response3 = await obtainInstruction("broccoli", 2)
+    document.querySelector("#broccoli").innerHTML += `<li>${response3}</li>`
+    const response4 = await obtainInstruction("broccoli", 3)
+    document.querySelector("#broccoli").innerHTML += `<li>${response4}</li>`
+    const response5 = await obtainInstruction("broccoli", 4)
+    document.querySelector("#broccoli").innerHTML += `<li>${response5}</li>`
+    const response6 = await obtainInstruction("broccoli", 5)
+    document.querySelector("#broccoli").innerHTML += `<li>${response6}</li>`
+    const response7 = await obtainInstruction("broccoli", 6)
+    document.querySelector("#broccoli").innerHTML += `<li>${response7}</li>`
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+  }
+  catch (err){
+    console.log(err)
+  }
+}
 
+makeBroccoli()
 
 
 // Bonus 2 - Promise all
